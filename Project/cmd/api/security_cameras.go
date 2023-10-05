1b9de6bdff5de2ddeee53858ff19c33e91a8fb1e
Baimyrza Lesbek <baimurza.lesbek@gmail.com>
2023-10-05 17:45:16 +0600

    added 3.6 chapter

--- Project/cmd/api/security_cameras.go
+++ Project/cmd/api/security_cameras.go
@@ -15,7 +15,7 @@ func (app *application) createSecurityCamerasHandler(w http.ResponseWriter, r *h
 func (app *application) showSecurityCamerasHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
@@ -32,7 +32,6 @@ func (app *application) showSecurityCamerasHandler(w http.ResponseWriter, r *htt
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"security_camera": securityCamera}, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
