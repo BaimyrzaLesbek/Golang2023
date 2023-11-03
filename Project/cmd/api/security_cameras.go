612836cefc8f0327fab9c5e08ff51af90bed6f93
Baimyrza Lesbek <baimurza.lesbek@gmail.com>
2023-11-03 18:52:43 +0600

    9.2 chapter added

--- Project/cmd/api/security_cameras.go
+++ Project/cmd/api/security_cameras.go
@@ -198,8 +198,9 @@ func (app *application) listSec_CamerasHandler(w http.ResponseWriter, r *http.Re
 	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
 
 	input.Filters.Sort = app.readString(qs, "sort", "id")
+	input.Filters.SortSafelist = []string{"id", "manufacturer", "resolution", "-id", "-manufacturer", "-resolution"}
 
-	if !v.Valid() {
+	if data.ValidateFilters(v, input.Filters); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
