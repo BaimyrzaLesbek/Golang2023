c61b8a0a621adce53ce6fa2818969f626d716b0a
Baimyrza Lesbek <baimurza.lesbek@gmail.com>
2023-11-05 17:58:30 +0600

    10.2 chapter added

--- Project/cmd/api/routes.go
+++ Project/cmd/api/routes.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
@@ -20,5 +20,5 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodPatch, "/v1/security_cameras/:id", app.updateSecurityCameraHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/security_cameras/:id", app.deleteSecurityCameraHandler)
 
-	return router
+	return app.recoverPanic(router)
 }
