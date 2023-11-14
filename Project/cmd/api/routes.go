79e19969e3ab52315440f33833de265fb4991db7
Baimyrza Lesbek <baimurza.lesbek@gmail.com>
2023-11-14 15:32:27 +0600

    13.3 chapter added

--- Project/cmd/api/routes.go
+++ Project/cmd/api/routes.go
@@ -20,5 +20,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/security_cameras/:id", app.updateSecurityCameraHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/security_cameras/:id", app.deleteSecurityCameraHandler)
 
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+
 	return app.recoverPanic(app.rateLimit(router))
 }
