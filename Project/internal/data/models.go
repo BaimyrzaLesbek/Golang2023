275bedbfd714da8a3d52a799cc430cc7fd3dc80a
Baimyrza Lesbek <baimurza.lesbek@gmail.com>
2023-11-20 18:34:54 +0600

    15.2 chapter added

--- Project/internal/data/models.go
+++ Project/internal/data/models.go
@@ -12,12 +12,14 @@ var (
 
 type Models struct {
 	SecurityCameras SecurityCameraModel
+	Tokens          TokenModel
 	Users           UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		SecurityCameras: SecurityCameraModel{DB: db},
+		Tokens:          TokenModel{DB: db},
 		Users:           UserModel{DB: db},
 	}
 }
