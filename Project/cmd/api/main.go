b98ca51430af9b9a775dfec4daf02c7f513f95c7
BaimyrzaLesbek <baimurza.lesbek@gmail.com>
2023-12-01 15:57:34 +0600

    18.3 chapter added

--- Project/cmd/api/main.go
+++ Project/cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 	_ "github.com/lib/pq"
 	"net/http"
 	"os"
+	"strings"
 	"sync"
 	"time"
 )
@@ -36,6 +37,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -68,6 +72,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "877e11ff25330d", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "0ff8ea5ef58feb", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.alexedwards.net>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
